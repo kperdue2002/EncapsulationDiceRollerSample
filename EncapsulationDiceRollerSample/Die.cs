36f817096fe9949740a82512f684ec46121da124
bobalobdob <kyranperdue@gmail.com>
2020-01-15 20:18:23 -0800

    Add project files.

---
+++ EncapsulationDiceRollerSample/Die.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDiceRollerSample
+{
+    /// <summary>
+    /// Represents a single six sided die
+    /// </summary>
+    class Die
+    {
+        //static members get shared across all instances of this class
+        private static Random rand;
+
+        /// <summary>
+        /// runs once before any die objects are created and initializes Random object once
+        /// </summary>
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
+        /// the current value of the die 1-6
+        /// </summary>
+        public byte FaceValue { get; private set; }
+
+        /// <summary>
+        /// die that are held are not rolled
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// generates a new random face value
+        /// sets FaceValue to the new value
+        /// returns generated value
+        /// </summary>
+        public byte Roll() {
+
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+
+            const byte minVal = 1;
+            const byte maxVal = 6;
+            //offset is for the exclusive range of Random.Next()
+            const byte offset = 1;
+            byte newValue = (byte)rand.Next(minVal, maxVal + offset);
+            FaceValue = newValue;
+            return newValue;
+        }
+    }
+}
---
+++ EncapsulationDiceRollerSample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EncapsulationDiceRollerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die[] dice = InitDice();
+            PrintDieFaceValues(dice);
+
+            //hold the first three dice
+            dice[0].IsHeld = true;
+            dice[1].IsHeld = true;
+            dice[2].IsHeld = true;
+
+            Console.WriteLine("\n\nholding first three.\n\n");
+            RollAllDice(dice);
+            PrintDieFaceValues(dice);
+
+            Console.ReadKey();
+        }
+
+        private static void RollAllDice(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i].Roll();
+            }
+        }
+
+        private static void PrintDieFaceValues(Die[] dice)
+        {
+            //print out the values
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine(dice[i].FaceValue);
+            }
+        }
+
+        /// <summary>
+        /// initializes all die in a given array
+        /// assumes array has been given a length
+        /// </summary>
+        /// <returns></returns>
+        private static Die[] InitDice()
+        {
+            //create five dice
+            Die[] dice = new Die[5];
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+
+            return dice;
+        }
+    }
+}
